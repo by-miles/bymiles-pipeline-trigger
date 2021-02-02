80a202a350f5971833dda053b3a5655fd9259993
n3op2 <paulius.michelevicius@gmail.com>
2021-02-02 12:06:34 +0000

    JOUR-149: using dist/index.js added node_modules back to gitignore.

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const AWS = require("aws-sdk");
 const core = require("@actions/core");
 
-try {
-    const awsAccessKey = core.getInput("aws-access-key");
-    const awssecretKey = core.getInput("aws-secret-key");
-    const pipelineName = core.getInput("pipeline-name");
-
-    AWS.config = new AWS.Config();
-    AWS.config.region = 'eu-west-2';
-    AWS.config.accessKeyId = awsAccessKey;
-    AWS.config.secretAccessKey = awssecretKey;
-
-    const codepipeline = new AWS.CodePipeline();
-
-    await codepipeline.startPipelineExecution({
-        name: pipelineName,
-    }).promise();
-
-} catch (error) {
-    core.setFailed(error.message);
-}
+(async() => {
+    try {
+        const awsAccessKey = core.getInput("aws-access-key");
+        const awssecretKey = core.getInput("aws-secret-key");
+        const pipelineName = core.getInput("pipeline-name");
+    
+        AWS.config = new AWS.Config();
+        AWS.config.region = 'eu-west-2';
+        AWS.config.accessKeyId = awsAccessKey;
+        AWS.config.secretAccessKey = awssecretKey;
+    
+        const codepipeline = new AWS.CodePipeline();
+    
+        await codepipeline.startPipelineExecution({
+            name: pipelineName,
+        }).promise();
+    
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
